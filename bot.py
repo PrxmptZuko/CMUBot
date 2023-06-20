6201082ad25054f6657895fcd20f09526d533c78
PrxmptZuko <maxwellhause724@gmail.com>
2023-06-20 15:06:14 -0400

    Working on auth and ticket functions

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
-import discord, responses, json
+import discord, discord.ext, responses, json, random
 from active_directory import ActiveDirectory
+from discord.ext import commands
+from discord.ui import Button, Select, View
+
 
 # Initializes bot and active directory sessions.
 bot = discord.Client(intents=discord.Intents.default())
 AD = ActiveDirectory()
+
+#defining prefix ! to run bot commands
+bot = commands.Bot(command_prefix="!")
+
+# store user data with user_id as the key and authentication code as the value
+user_data = {}
 
 # independant message sent by the bot in either a private DM or returning output to original channel
 async def send_message(message, user_message, is_private):
@@ -34,11 +43,120 @@ client, TOKEN = run_discord_bot()
 async def on_ready():
     print(f'{client.user} is now running!')
 
-# barebone user join logic - needs authentication process to prompt to user in private DM
+
+
+
+# join logic - needs authentication process to prompt to user in private DM
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined the server!')
+    welcome_message = "Welcome to the Official Central Michigan Esports Discord! Please provide your email address for authentication:"
+    try:
+        await member.send(welcome_message)
+    except discord.Forbidden:
+        print(f"Failed to send user a private message to {member}.")
+
+@client.event 
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    # check if the message is from a new user in private message
+    if isinstance(message.channel, discord.DMChannel) and message.author != client.user:
+        user_id = message.author.id
+
+        #check if the user already has auth code
+        if user_id in user_data:
+            await authentication_user(message, user_id)
+        else: 
+            await generate_authentication_code(message, user_id)
+
+async def generate_authentication_code(message, user_id):
+    email_address = message.content
+
+    # Generate random 5 digit code for auth
+    authentication_code = random.randint(10000, 99999)
+
+    # store the authentication code for the user
+    user_data[user_id] = authentication_code
+
+    #send the Authentication code to the user via email
+    subject = "Authentication Code"
+    email_message = (f"Your authentication code is: {authentication_code}")
+    await send_email(email_address, subject, email_message)
+
+    #reply to the user in the private message
+    reply_message = "Thank you for providing your email address. " \
+                    "An email has been sent to you with the authentication code."
+    await message.channel.send(reply_message)
+
+async def authentication_user(message, user_id):
+    authentication_code = int(message.content)
+    stored_authentication_code = user_data[user_id]
+
+    if authentication_code == stored_authentication_code:
+        #user is authenticated
+        del user_data[user_id] # Remove the authenticatoin code from the user_data
+        success_message ="Authentication successful! You are now verified! Welcome to CMU ESPORTs Discord ({member})"
+    else:
+        #invalid auth code
+        failure_message = "Authentication failed. Please make sure to enter the correct code"
+        await message.channel.send(failure_message)
+
+def send_email(email_address, subject, message):
+    print(f"Sending email to {email_address}: \nSubject: {subject}\nMessage: {message}")
+
+
+
+# Help Ticket creation 
+
+
+# defaults users cant see channel, when interaction (user select create ticket) changes view permission to 'True'. role = True allows mods to see channel
+async def ticketcallback(interaction):
+    guild = interaction.guild
+    role = discord.utils.get(guild.roles, name="Moderator")
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(view_channel=False),
+        interaction.user: discord.PermissionOverwrite(view_channel=True),
+        role: discord.PermissionOverwrite(view_channel=True)
+    }
+
+# user selects either option 1 or 2 being a help ticket or something else (test)
+     #edit : changed 'Select' to discord.ui.select
+    select = discord.ui.select (options = [
+        discord.SelectOption(label="Help Ticket", value="01", emoji="✅", description= "This will open a help ticket"),
+        discord.SelectOption(label="Other Ticket", value="02", emoji="❌", description="This will open a ticket in the other section")
+    ])
+
+# Handles ticket channel creation and sets necessary permissions 
+async def my_callback(interaction):
+    if select.value[0] == "01":
+        category = discord.utils.get(guild.categories, name="Tickets")
+        channel = await guild.create_text_channel(f"{interaction.user.name}-ticket", category=category, overwrites=overwrites)
+        await interaction.response.send_message(f"Created ticket - <#{channel.id}>", ephemeral=True)
+        await channel.send("Hello, how can i help you?")
+    elif select.values[0] == "02":
+        category = discord.utils.get(guild.categories, name= "Other Tickets")
+        channel = await guild.create_text_channel(f"{interaction.user.name}-ticket", category=category, overwrites=overwrites)
+        await interaction.response.send_message(f"Created ticket - <#{channel.id}>", ephemeral=True)
+        await channel.send("Hello, how can i help you?")
+
+    select.callback = my_callback
+    view = View(timeout=None)
+    view.add_item(select)
+    await interaction.response.send_message("Choose an option below", view=view, ephemeral=True)
+
+# I want to test ctx (context) instead of labeling individual values. 'ctx' can pull details within commands so i want to try 
+@bot.command()
+async def ticket(interaction):
+    button = Button(label= "📥 Create Ticket", style=discord.ButtonStyle.green)
+    button.callback = ticketcallback
+    view = discord.ui.View(timeout=None)
+    view.add_item(button)
+    await interaction.send("Open a ticket below", view=view)
+
+
+
 
 # When message is sent into a channel that the bot has access to -
 # @client.event logs user chat msg to check if its a "!command", if so, Calls the 'send_message' function to send response to user
--- responses.py
+++ responses.py
@@ -1,5 +1,7 @@
 import random
 
+MOD_CHANNEL_ID = 123
+
 def handle_response(message, AD='') -> str:
     """Basic response functionality for bot"""
     p_message = message.lower()
@@ -23,8 +25,20 @@ def handle_response(message, AD='') -> str:
             return f'An error has occured: {error}'
         else:
             return f"User: {user}\nDescription: {user_description}"
+        
 
-    if p_message == '!help':
-        # TODO Add options to contact a mod or create a support ticket
+async def on_message(message):
+    if message.author == Client.user:
+        return
+    
+        # ticketing to send msg to mod channel 
+    if message == '!help':
+        if any(role.name == 'Moderator' for role in message.author.roles):
+            mod_channel = Client.get_channel(MOD_CHANNEL_ID)
+            await mod_channel.send(f' Ticket request from {message.author.mention}')
+            await message.author.send('Your ticket request has been submitted. A moderator will assist you shortly.')
+        else:
+            await message.channel.send('This command is only available to moderators.')
         return "'This is a help message that you can modify.'"
     
+
