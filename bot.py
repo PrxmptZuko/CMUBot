e4fddbc123ff529d509b1ecd0cfd8eb7d80089b5
SadNarwhal27 <schallt2015@gmail.com>
2023-06-28 12:17:33 -0400

    Created the user_authentication file

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
-import discord, discord.ext, responses, json, random
+import discord, responses, json, random
 from active_directory import ActiveDirectory
 from discord.ext import commands
-from discord.ui import Button, Select, View
+from discord.ui import Button, View
+
+import user_authentication
 
 
 # Initializes bot and active directory sessions.
 bot = discord.Client(intents=discord.Intents.default())
 AD = ActiveDirectory()
 
-#defining prefix ! to run bot commands
-bot = commands.Bot(command_prefix="!")
+#defining prefix ! to run bot 
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # store user data with user_id as the key and authentication code as the value
 user_data = {}
@@ -28,7 +31,7 @@ def get_token_from_file(TOKEN):
         token = data["TOKEN"]
         return token
 
-#Activates bot - creates instance of the client and preps connection to Discord API with TOKEN and outlines permissions
+# Activates bot
 def run_discord_bot():
     TOKEN = get_token_from_file("config.json")
     intents = discord.Intents.default()
@@ -52,9 +55,10 @@ async def on_ready():
 async def on_member_join(member):
     welcome_message = "Welcome to the Official Central Michigan Esports Discord! Please provide your email address for authentication:"
     try:
+        user_authentication.create_user(member)
         await member.send(welcome_message)
     except discord.Forbidden:
-        print(f"Failed to send user a private message to {member}.")
+        print(f"Failed to send a private message to {member}.")
 
 @client.event 
 async def on_message(message):
@@ -66,45 +70,26 @@ async def on_message(message):
         user_id = message.author.id
 
         #check if the user already has auth code
-        if user_id in user_data:
-            await authentication_user(message, user_id)
+        with open('users.json', 'w') as file:
+            user_data = file.readlines()
+
+        if user_id not in user_data:
+            email_package = user_authentication.create_email(message)
+            user_data[user_id]['email'] = email_package['email_address']
+            user_data[user_id]['auth_code'] = email_package['auth_code']
+            file.close()
+
+            user_authentication.send_email(email_package)
+
+            reply_message = ("Thank you for providing your email address.\n"
+                            f"An email has been sent to {email_package['email_address']} with your authorization code")
+            await message.channel.send(reply_message)
         else: 
-            await generate_authentication_code(message, user_id)
+            reply_message, role = user_authentication.authenticate_user(message, AD)
 
-async def generate_authentication_code(message, user_id):
-    email_address = message.content
+            # TODO assign a user role based on the role variable
 
-    # Generate random 5 digit code for auth
-    authentication_code = random.randint(10000, 99999)
-
-    # store the authentication code for the user
-    user_data[user_id] = authentication_code
-
-    #send the Authentication code to the user via email
-    subject = "Authentication Code"
-    email_message = (f"Your authentication code is: {authentication_code}")
-    await send_email(email_address, subject, email_message)
-
-    #reply to the user in the private message
-    reply_message = "Thank you for providing your email address. " \
-                    "An email has been sent to you with the authentication code."
-    await message.channel.send(reply_message)
-
-async def authentication_user(message, user_id):
-    authentication_code = int(message.content)
-    stored_authentication_code = user_data[user_id]
-
-    if authentication_code == stored_authentication_code:
-        #user is authenticated
-        del user_data[user_id] # Remove the authenticatoin code from the user_data
-        success_message ="Authentication successful! You are now verified! Welcome to CMU ESPORTs Discord ({member})"
-    else:
-        #invalid auth code
-        failure_message = "Authentication failed. Please make sure to enter the correct code"
-        await message.channel.send(failure_message)
-
-def send_email(email_address, subject, message):
-    print(f"Sending email to {email_address}: \nSubject: {subject}\nMessage: {message}")
+            await message.channel.send(reply_message)
 
 
 
@@ -121,15 +106,15 @@ async def ticketcallback(interaction):
         role: discord.PermissionOverwrite(view_channel=True)
     }
 
-# user selects either option 1 or 2 being a help ticket or something else (test)
+    # user selects either option 1 or 2 being a help ticket or something else (test)
      #edit : changed 'Select' to discord.ui.select
     select = discord.ui.select (options = [
         discord.SelectOption(label="Help Ticket", value="01", emoji="✅", description= "This will open a help ticket"),
         discord.SelectOption(label="Other Ticket", value="02", emoji="❌", description="This will open a ticket in the other section")
     ])
 
-# Handles ticket channel creation and sets necessary permissions 
-async def my_callback(interaction):
+    # Handles ticket channel creation and sets necessary permissions 
+
     if select.value[0] == "01":
         category = discord.utils.get(guild.categories, name="Tickets")
         channel = await guild.create_text_channel(f"{interaction.user.name}-ticket", category=category, overwrites=overwrites)
@@ -141,7 +126,6 @@ async def my_callback(interaction):
         await interaction.response.send_message(f"Created ticket - <#{channel.id}>", ephemeral=True)
         await channel.send("Hello, how can i help you?")
 
-    select.callback = my_callback
     view = View(timeout=None)
     view.add_item(select)
     await interaction.response.send_message("Choose an option below", view=view, ephemeral=True)
@@ -172,7 +156,7 @@ async def on_message(message):
 
     print(f'{username} said: "{user_message}" ({channel})')
 
-    if user_message[0] == '?':
+    if user_message.startswith('?'):
         user_message = user_message[1:] 
         await send_message(message, user_message, is_private=True)
     else:
@@ -180,3 +164,6 @@ async def on_message(message):
 
 
 client.run(TOKEN)
+
+if __name__ == '__main__':
+    run_discord_bot()
---
+++ user_authentication.py
@@ -0,0 +1,90 @@
+import re
+import random
+import json
+
+def _generate_authentication_code():
+    """Creates an authentication code"""
+
+    auth_code = random.randint(10000, 99999)
+    return auth_code
+
+def _find_authentication_code(message):
+    """Looks for the 5 digit auth code within a message"""
+
+    auth_code = re.search(r"[0-9]{5}", message.content).group()
+    return auth_code
+
+def create_user(member):
+    """Creates a user record within the user.json file"""
+
+    # Acts as a placeholder for future data provided by user
+    user_record = str(member.author.id)
+    user_record = {
+        'email': '',
+        'auth_code': ''
+    }
+    
+    # Opens the users.json file and appends a new user to the list
+    with open('users.json', 'a') as file:
+        json.dump(user_record, file)
+        file.close()
+    print('User record created')
+    
+
+def create_email(message):
+    """Creates the email contents to be sent out"""
+
+    # Grabs the provided email from the user's message using regex
+    email_address = re.search(r"[a-z0-9._%+-]+\@[a-z0-9._%+-]+\.[a-z]{3}", message.content.lower()).group()
+    auth_code = _generate_authentication_code()
+
+    email_subject = "[Do Not Reply] - CMU eSports Authentication Code"
+    email_body = f"Your authentication code is: \n{auth_code}"
+
+    # This package is used for both the sent email and user info
+    email_package = {
+        'email_address': email_address,
+        'email_subject': email_subject,
+        'email_body': email_body,
+        'auth_code': auth_code
+    }
+
+    return email_package
+
+def send_email(email_package):
+    """Placeholder for real email functionality later"""
+
+    # TODO Add real functionality to this method
+    print(f"Email sent to {email_package['email_address']}\nSubject: {email_package['email_subject']}\Body: {email_package['email_body']}")
+
+def authenticate_user(message, AD):
+    """Looks in the users.json file for an existing user and authenticates from there"""
+    
+    # Gets the current user from the users.json file
+    with open('users.json', 'r') as file:
+        user_data = file.readlines
+        current_user = user_data[message.author.id]
+        file.close()
+
+    # Confirms the authentication code exists within the message
+    auth_code = int(_find_authentication_code(message))
+    if auth_code == None: 
+        output_message = "Authentication failed. Please make sure to enter the correct code" 
+        return output_message, None
+    
+    # Matches the provided auth_code with the user's stored auth_code
+    if current_user['auth_code'] == auth_code:
+        output_message = f"Authentication successful! Your account has been verified\nWelcome to CMU eSports Discord {message.user_id}"
+        
+        # Grabs the users email and checks if they used a cmich.edu email using regex
+        user_email_segments = re.split(r'\@', current_user['email'])
+        if user_email_segments[1] == 'cmich.edu':
+            user_description = AD.look_up_user(user_email_segments[0])
+        else:
+            user_description = 'Community'
+    else:
+        output_message = "Authentication failed. Please make sure to enter the correct code" 
+        user_description = None
+    
+    # The message to the user and necessary role based on user description within AD
+    return output_message, user_description
